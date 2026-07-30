69e5acc2b1c0f57ed207d8b9b755adc6d41c56d1
durgeshkhushlani <beanonyfreak@gmail.com>
2026-07-30 15:12:16 +0530

    Version 2: remove legacy MERN implementation, add rebuild plan and dev environment config

--- backend/utils/generateCode.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-const generateUniversityCode = () => {
-  return crypto.randomBytes(4).toString('hex').toUpperCase();
-};
-
-module.exports = generateUniversityCode;
